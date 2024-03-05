69f14b7903d5d985b05957912b5dcdda4f2aadc8
SammyNA <sammy9na@gmail.com>
2024-03-05 01:25:02 -0800

    added all queries and transforms to milestone4, same functionality, just added all of the required queries and transforms

--- StringSearchMilestone4.java
+++ StringSearchMilestone4.java
@@ -15,19 +15,36 @@ interface Query{
     boolean matches(String s);
 }
 
-interface Transform {
-    String transform(String s);
-}
-
-class Starts implements Query {
-    String start;
-
-    Starts(String start) {
-        this.start = start.substring(start.indexOf("'") + 1, start.lastIndexOf("'"));
+class Length implements Query {
+    String num;
+    Length(String num){
+        this.num = num;
     }
 
     public boolean matches(String s) {
-        return s.startsWith(start);
+        return s.length() == Integer.parseInt(this.num);
+    }
+}
+
+class Greater implements Query {
+    String num;
+    Greater(String num){
+        this.num = num;
+    }
+
+    public boolean matches(String s) {
+        return s.length() > Integer.parseInt(this.num);
+    }
+}
+
+class Less implements Query {
+    String num;
+    Less(String num){
+        this.num = num;
+    }
+
+    public boolean matches(String s) {
+        return s.length() < Integer.parseInt(this.num);
     }
 }
 
@@ -39,6 +56,58 @@ class ContainsQuery implements Query{
     }
     public boolean matches(String s) {
         return s.contains(this.str);
+    }
+}
+
+class Starts implements Query {
+    String start;
+
+    Starts(String start) {
+        this.start = start.substring(start.indexOf("'") + 1, start.lastIndexOf("'"));
+    }
+
+    public boolean matches(String s) {
+        return s.indexOf(this.start) == 0;
+    }
+}
+
+class Ends implements Query {
+    String end;
+    Ends(String end) {
+        this.end = end.substring(end.indexOf("'") + 1, end.lastIndexOf("'"));
+    }
+
+    public boolean matches(String s) {
+        return s.endsWith(end);
+    }
+}
+
+class Not implements Query {
+    String str;
+    Not(String str) {
+        this.str = str;
+    }
+
+    public boolean matches(String s) {
+        Query q = StringSearch.readQuery(str);
+        return q.matches(s) == false;
+    }
+}
+
+interface Transform {
+    String transform(String s);
+}
+
+class Upper implements Transform {
+
+    public String transform(String s) {
+        return s.toUpperCase();
+    }
+}
+
+class Lower implements Transform {
+    public String transform(String s) {
+        return s.toLowerCase();
     }
 }
 
@@ -58,22 +127,68 @@ class First implements Transform {
     }
 }
 
+class Last implements Transform {
+    String num;
+    Last(String num) {
+        this.num = num;
+    }
+
+    public String transform(String s) {
+        int val = Integer.parseInt(this.num);
+        if (s.length() < val) {
+            return s;
+        } else {
+            return s.substring(s.length() - val, s.length());
+        }
+    }
+}
+
+class Replace implements Transform {
+    String str;
+    String str1;
+    Replace(String str, String str1) {
+        this.str = str.substring(str.indexOf("'") + 1, str.lastIndexOf("'"));
+        this.str1 = str1.substring(str1.indexOf("'") + 1, str1.lastIndexOf("'"));
+    }
+
+    public String transform(String s) {
+        return s.replace(str, str1);
+    }
+}
+
 class StringSearch{
 
     static Query readQuery(String q) {
-        String[] queryParts = q.split("=");
-        if (queryParts.length < 2) {
+
+        String query = "";
+        String value = "";
+
+        if (q.contains("=") && q.contains("(")) {
+            query += q.substring(0, q.indexOf("("));
+            value += q.substring(q.indexOf("(") + 1, q.indexOf(")"));
+        } else if (q.contains("=") == true) {
+            String[] queryParts = q.split("=");
+            query += queryParts[0].trim(); // for queries w/ no = can do if elif else where these are created in those statements
+            value += queryParts[1].trim();
+        } else {
             System.out.println("Invalid query, please try again");
         }
 
-        String query = queryParts[0].trim(); // for queries w/ no = can do if elif else where these are created in those statements
-        String value = queryParts[1].trim();
-
         switch (query) {
-            case "Starts":
+            case "starts":
                 return new Starts(value);
-            case "Contains":
+            case "contains":
                 return new ContainsQuery(value);
+            case "length":
+                return new Length(value);
+            case "greater":
+                return new Greater(value);
+            case "less":
+                return new Less(value);
+            case "ends":
+                return new Ends(value);
+            case "not":
+                return new Not(value);
             default:
                 System.out.println("Unsupported query type: " + query);
                 return null;
@@ -81,19 +196,34 @@ class StringSearch{
     }
 
     static Transform readTransform(String t) {
-        String[] transformParts = t.split("=");
-        if (transformParts.length < 2) {
+
+        String transform = "";
+        String value = "";
+
+        if (t.contains("=") != true) {
+            transform += t;
+        } else if (t.contains("=") == true) {
+            String[] transformParts = t.split("=");
+            transform += transformParts[0].trim(); // for queries w/ no = can do if elif else where these are created in those statements
+            value += transformParts[1].trim();
+        } else {
             System.out.println("Invalid query, please try again");
         }
 
-        String transform = transformParts[0].trim(); // for queries w/ no = can do if elif else where these are created in those statements
-        String value = transformParts[1].trim();
-
         switch (transform) {
-            case "First":
+            case "upper":
+                return new Upper();
+            case "lower":
+                return new Lower();
+            case "first":
                 return new First(value);
+            case "last":                // "First" and "Last" need to be fixed, last/first characters
+                return new Last(value); // not last/first words
+            case "replace":
+                String[] vals = value.split(";");
+                return new Replace(vals[0], vals[1]);
             default:
-                System.out.println("Unsupported query type: " + transform);
+                System.out.println("Unsupported transform type: " + transform);
                 return null;
         }
     }
@@ -125,8 +255,6 @@ class StringSearch{
                     System.out.println(output);
                 }
             }
-
-
         }
     }
 }
